0a15b630f888265afeea10f8e9af81b370b0f573
First Wanderer <kaa-master37@mail.ru>
2016-04-24 15:09:14 +0300

    Update constructor of photo, delete prototip methods

--- src/photo.js
+++ src/photo.js
@@ -3,23 +3,24 @@
 var showGallery = require('./gallery').showGallery;
 var getPictureElement = require('./get-picture-element');
 
+
 var Photo = function(data, number, container) {
   this.data = data;
   this.number = number;
   this.element = getPictureElement(data, container);
 
-  this.element.addEventListener('click', this.onPhotoClick.bind(this));
+  this.onPhotoClick = (function(evt) {
+    evt.preventDefault();
+    showGallery(this.number);
+  }).bind(this);
+
+  this.remove = function() {
+    this.element.removeEventListener('click', this.onPhotoClick);
+    this.element.parentNode.removeChild(this.element);
+  };
+
+  this.element.addEventListener('click', this.onPhotoClick);
   container.appendChild(this.element);
-};
-
-Photo.prototype.onPhotoClick = function(evt) {
-  evt.preventDefault();
-  showGallery(this.number);
-};
-
-Photo.prototype.remove = function() {
-  this.element.removeEventListener('click', this.onPhotoClick.bind(this));
-  this.element.parentNode.removeChild(this.element);
 };
 
 module.exports = Photo;
